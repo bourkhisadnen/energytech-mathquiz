21e7d71244260f05455345b549ca7ba14938f5e3
bourkhisadnen <bourkhisadnen@gmail.com>
2026-09-15 16:27:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let currentSession = null;
 let activeRole = 'teacher';
 let studentSubmitted = false;
 
-const DEFAULT_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwG6sb3Dn8MCETk5uphIZ4p6BelbLu1IgbayrpQiIKeWCumLio5roK4gpMDDOh3O_X6/exec'
+const DEFAULT_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzqpiZRw1mofWof5PLSpzbXXbI4xutnrBhayyVvtoUcYs5O3REAZjP97Vi1nFqiRJQ6/exec'
 const WEB_APP_URL_STORAGE_KEY = 'energytechWebAppUrl_v2';
 const AUTH_TOKEN_KEY = 'energytechAuthToken_v1';
 const AUTH_USER_KEY = 'energytechAuthUser_v1';
